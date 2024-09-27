7d35e66b629942d436a451dee6c159b3f220e1a5
Pierre de Yzaguirre <p.deyzaguirre@gmail.com>
2024-09-27 11:16:05 +0200

    initial commit

--- src/components/FooterCV.vue
+++ src/components/FooterCV.vue
@@ -1,13 +1,15 @@
 <template>
   <footer class="green-bg-ext py-4" id="cv">
-          <div class="container mx-auto text-center">
-              <a href="/cv.pdf" download class="green-btn font-bold py-2 px-4 rounded">
-                  Télécharger le CV
-              </a>
-          </div>
-      </footer>
+    <div class="container mx-auto text-center">
+      <a
+        href="DE YZAGUIRRE Pierre CV.pdf"
+        download
+        class="green-btn font-bold py-2 px-4 rounded"
+      >
+        Télécharger le CV
+      </a>
+    </div>
+  </footer>
 </template>
 
-<script>
-
-</script>
+<script></script>
